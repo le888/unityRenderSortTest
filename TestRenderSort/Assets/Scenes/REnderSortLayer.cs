4daaa4bf879740a6fe114a131288b49fb7378538
le888 <707649841@qq.com>
2020-05-14 14:08:32 +0800

    REnderSort Test

---
+++ TestRenderSort/Assets/Scenes/REnderSortLayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class REnderSortLayer : MonoBehaviour
+{
+    public Renderer render1;
+    public Renderer render2;
+
+    public string sortingLayer1 = "Default";
+    public string sortingLayer2 = "Default";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        render1.sortingLayerID = SortingLayer.NameToID(sortingLayer1);
+        render2.sortingLayerID = SortingLayer.NameToID(sortingLayer2);
+    }
+}
---
+++ TestRenderSort/Assets/Scenes/RenderSortTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class RenderSortTest : MonoBehaviour
+{
+    public Renderer render1;
+    public Renderer render2;
+
+    public int sortingOrder1 = 0;
+    public int sortingOrder2 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        render1.sortingOrder = sortingOrder1;
+        render2.sortingOrder = sortingOrder2;
+    }
+}
